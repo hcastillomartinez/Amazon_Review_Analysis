f14878fa93285b7fcd08dba561a1e2795e3a6ed3
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2019-11-24 22:53:44 -0700

    Limiting the number of rows t 500,000 to speed up the amount of data we had. As it sits it is 100 MB ~ 1 hour.

--- src/reader.py
+++ src/reader.py
@@ -1,23 +1,67 @@
 from textblob import TextBlob
 import csv
-import sys
+import os
 
+
+import sys
+#C:\Users\damia\PycharmProjects\Amazon_Review_Analysis\src
 csv.field_size_limit(100000000)
-with open(sys.argv[1],'r',encoding='Latin-1') as tsvfile, open('modified_'+sys.argv[1],'w',encoding='Latin-1') as tsvfileout:
-    reader = csv.reader(tsvfile, delimiter='\t',quoting=csv.QUOTE_NONE)
-    writer = csv.writer(tsvfileout, delimiter='\t', lineterminator='\n')
-    row_head = 1
-    for row in reader:
-        #Writing the headers
-        if row_head == 1:
-            row.append('sentiment_rating')
-            row.append('subjectivity_rating')
-            row_head = 0
-            writer.writerow(row)
-        else:
-            #Writing the other columns
-            review = TextBlob(row[13])
-            review.sentiment
-            row.append(review.sentiment.polarity)
-            row.append(review.sentiment.subjectivity)
-            writer.writerow(row)
+directory = r'C:\Users\damia\PycharmProjects\Amazon_Review_Analysis\data'
+row_max = 500000
+for filename in os.listdir(directory):
+    out = "C:\\Users\\damia\\PycharmProjects\\Amazon_Review_Analysis\\data\\"
+    out2 = "C:\\Users\\damia\\PycharmProjects\\Amazon_Review_Analysis\\mod_data\\"
+    out3 = "D:\\"
+    with open(out+filename,'r',encoding='Latin-1') as tsvfile, open(out2+'new_modified_'+filename,'w',encoding='Latin-1') as tsvfileout:
+        curr_row = 0
+        reader = csv.reader(tsvfile, delimiter='\t',quoting=csv.QUOTE_NONE)
+        writer = csv.writer(tsvfileout, delimiter='\t', lineterminator='\n')
+        row_head = 1
+
+        for row in reader:
+            #Writing the headers
+            if row_head == 1:
+                row.append('word_count')
+                row.append('sentence_count')
+                row.append('adjective_count')
+                row.append('adverb_count')
+                row.append('noun_count')
+                row.append('pronoun_count')
+                row.append('verb_count')
+                row_head = 0
+                writer.writerow(row)
+            else:
+                #Writing the other columns
+                review = TextBlob(row[13])
+                tags = review.tags
+                sentences = review.sentences
+                adjectives = 0
+                adverbs = 0
+                nouns = 0
+                pronouns = 0
+                verb = 0
+                sentence_count = len(review.sentences)
+                word_count = len(review.words)
+                for n in tags:
+                    if n[1] == "JJ" or n[1] == "JJR" or n[1] == "JJS":
+                        adjectives += 1
+                    if n[1] == "RB" or n[1] == "RBR" or n[1] == "RBS" or n[1] == "RP":
+                        adverbs += 1
+                    if n[1] == "PRP" or n[1] == "PRP$":
+                        nouns += 1
+                    if n[1] == "JJ" or n[1] == "JJR" or n[1] == "JJS":
+                        pronouns += 1
+                    if n[1] == "MD" or n[1] == "VB" or n[1] == "VBZ" or n[1] == "VBP" or n[1] == "VBD" or n[
+                        1] == "VBN" or n[1] == "VBG":
+                        verb += 1
+                row.append(word_count)
+                row.append(sentence_count)
+                row.append(adjectives)
+                row.append(adverbs)
+                row.append(nouns)
+                row.append(pronouns)
+                row.append(verb)
+                curr_row += 1
+                writer.writerow(row)
+                if curr_row >= row_max:
+                    break

ca40bf7df309418d65002a9c5d2d2a9083fd7253
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2019-12-06 00:03:27 -0700

    Update sample_generator.py to handle duplicate entries. Each sample is disjoint from other samples.

--- src/sample_generator.py
+++ src/sample_generator.py
@@ -2,12 +2,15 @@ import random
 import os
 import csv
 
-p = 0.35
+p = .35
 directory = r'C:\Users\damia\PycharmProjects\Amazon_Review_Analysis\data'
+# directory = r'C:\Users\damia\Desktop\new_samples'
 out = "C:\\Users\\damia\\PycharmProjects\\Amazon_Review_Analysis\\data\\"
+# out = "C:\\Users\\damia\\Desktop\\new_samples\\"
 out2 = "C:\\Users\\damia\\PycharmProjects\\Amazon_Review_Analysis\\mod_data\\"
 header = 1
-sample = 8
+sample = 2
+seen = []
 for x in range(sample):
     for filename in os.listdir(directory):
         header = 1
@@ -21,5 +24,9 @@ for x in range(sample):
                     header = 0
                 else:
                     if (random.random()) > p:
-                        writer.writerow(row)
+                        if row[2] in seen:
+                            print("dup not added")
+                        else:
+                            writer.writerow(row)
+                            seen.append(row[2])
 

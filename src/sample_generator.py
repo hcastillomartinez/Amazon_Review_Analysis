b11ef2aaf1ed729c7280d897b9c53fa830148282
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2019-12-06 22:31:09 -0700

    Should only be used to generate sample at a time, wanted to simulate people maybe writing duplicate reviews

--- src/sample_generator.py
+++ src/sample_generator.py
@@ -9,24 +9,22 @@ out = "C:\\Users\\damia\\PycharmProjects\\Amazon_Review_Analysis\\data\\"
 # out = "C:\\Users\\damia\\Desktop\\new_samples\\"
 out2 = "C:\\Users\\damia\\PycharmProjects\\Amazon_Review_Analysis\\mod_data\\"
 header = 1
-sample = 2
+sample = 1
 seen = []
 for x in range(sample):
+    tsvfileout = open(out2 + 'no_dup_sample_' + str(x) + '.tsv', 'w', encoding='Latin-1')
+    writer = csv.writer(tsvfileout, delimiter='\t', lineterminator='\n')
     for filename in os.listdir(directory):
         header = 1
-        with open(out + filename, 'r', encoding='Latin-1') as tsvfile, open(out2 + 'sample_'+str(x)+'.tsv', 'w',
-                                                                            encoding='Latin-1') as tsvfileout:
-            reader = csv.reader(tsvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
-            writer = csv.writer(tsvfileout, delimiter='\t', lineterminator='\n')
-            for row in reader:
-                if header == 1:
+        print(filename)
+        print("Current sample %d" % x)
+        tsvfile = open(out + filename, 'r', encoding='Latin-1')
+        reader = csv.reader(tsvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            if header == 1:
+                writer.writerow(row)
+                header = 0
+            else:
+                if (random.random()) > p:
                     writer.writerow(row)
-                    header = 0
-                else:
-                    if (random.random()) > p:
-                        if row[2] in seen:
-                            print("dup not added")
-                        else:
-                            writer.writerow(row)
-                            seen.append(row[2])
 
